db2ad46922db1c9f99b5be31475463768916dd74
inkeybow2002 <ogs3rvl161k6@hotmail.com>
2025-08-29 03:24:13 +0100

    feat: initial commit for src/App.js

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Minimalist Web Dashboard</h1>
+        <p>Track your habits and goals seamlessly!</p>
+      </header>
+      {/* Additional components will go here */}
+    </div>
+  );
+}
+
+export default App;
